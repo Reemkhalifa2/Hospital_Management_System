4cb0b34737d938ef161bce10abdab1dada257a13
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 10:02:46 +0400

    Implement constructor chaining to call parent constructor (super keyword)

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -4,4 +4,7 @@ import java.time.LocalDate;
 
 public class Patient extends Person{
 
+    public Patient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+    }
 }
