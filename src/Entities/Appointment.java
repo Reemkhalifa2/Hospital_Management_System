c8d89d60b959bcd7d9979a5523397633baacd9c2
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 14:13:40 +0400

    add required fields

--- src/Entities/Appointment.java
+++ src/Entities/Appointment.java
@@ -1,4 +1,18 @@
 package Entities;
 
+import java.time.LocalDate;
+
 public class Appointment {
+
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate appointmentDate;
+    private String appointmentTime;
+    private String status;
+    private String reason;
+    private String notes;
+
+
+
 }
