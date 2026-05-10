21c0e891f9bc6a1eb0e7895df1997ebe202063c4
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 14:44:49 +0400

    Add method to add a patient in PatientService

--- src/Services/PatientService.java
+++ src/Services/PatientService.java
@@ -1,5 +1,19 @@
 package Services;
 
+import Entities.Patient;
+import Utilities.Constants;
+
+import java.io.Console;
+import java.util.List;
+
 public class PatientService {
+    private List<Patient> patients;
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+        System.out.println(Constants.PATIENT_ADDED_SUCCESSFULLY);
+    }
+
+
+
 
 }
