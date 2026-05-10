a67ed9472431bae9317758c19817d6ff493359e2
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 09:53:10 +0400

    Create a Person base class with fields

--- src/Entities/Person.java
+++ src/Entities/Person.java
@@ -1,4 +1,15 @@
 package Entities;
 
+import java.time.LocalDate;
+
 public class Person {
+    String id;
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    String gender;
+    String phoneNumber;
+    String email;
+    String address;
+
 }
