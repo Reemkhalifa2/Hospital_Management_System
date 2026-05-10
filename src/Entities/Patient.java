bdbe36bb01186d1c94dd53117d2e39c760dd636e
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 10:34:23 +0400

    Add specific fields for patient class

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -1,10 +1,22 @@
 package Entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Patient extends Person{
 
     public Patient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+
     }
+    String patientId;
+    String bloodGroup;
+    List<String> allergies;
+    String emergencyContact;
+    LocalDate registrationDate;
+    String insuranceId;
+    List<String> medicalRecords;
+    List<String> appointments;
+
+
 }
