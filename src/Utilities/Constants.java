2b9f117e558656d49779371030d7681c09914514
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 14:45:04 +0400

    add new Constants

--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static String ENTER_INSURANCE = "Enter new Insurance: ";
     public static String ASSIGNED = "Patient assigned.";
     public static String PATIENT_REMOVED = "Patient removed";
+    public static String PATIENT_ADDED_SUCCESSFULLY = "Patient added successfully";
     public static String AVAILABILITY_UPDATED = "Availability updated";
 
 
