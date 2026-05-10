b53762039fca0d3131f8f155caf49d3197f52001
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 12:53:31 +0400

    Implement add patient and assign patient method

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -33,6 +33,74 @@ public class Doctor extends Person{
         this.availableSlots = new ArrayList<>();
         this.assignedPatients = new ArrayList<>();
     }
+    public Patient addPatient(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter patient id:");
+        String id = scanner.nextLine();
+
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter gender:");
+        String gender = scanner.nextLine();
+
+        System.out.println("Enter phone number:");
+        String phone = scanner.nextLine();
+
+        System.out.println("Enter date of birth (yyyy-MM-dd):");
+        String DOB = scanner.nextLine();
+        LocalDate dateOfBirth = LocalDate.parse(DOB);
+
+        System.out.println("Enter email:");
+        String email = scanner.nextLine();
+
+        System.out.println("Enter address:");
+        String address = scanner.nextLine();
+
+        System.out.println("Enter patient ID:");
+        String patientId = scanner.nextLine();
+
+        System.out.println("Enter blood group:");
+        String bloodGroup = scanner.nextLine();
+
+        System.out.println("Enter emergency contact:");
+        String emergencyContact = scanner.nextLine();
+
+        System.out.println("Enter insurance ID:");
+        String insuranceId = scanner.nextLine();
+
+        Patient patient;
+        patient = new Patient(
+                id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phone,
+                email,
+                address,
+                patientId,
+                bloodGroup,
+                emergencyContact,
+                LocalDate.now(),
+                insuranceId
+        );
+        return patient;
+
+    }
+
+    public void assignPatient(){
+
+       assignedPatients.add(addPatient());
+
+    }
+
+
+
 
     @Override
     public void displayInfo() {
@@ -50,7 +118,7 @@ public class Doctor extends Person{
         }
 
         System.out.println("Assigned Patients");
-        for(String patient : assignedPatients){
+        for(Patient patient : assignedPatients){
             System.out.print(patient + ",");
         }
     }
