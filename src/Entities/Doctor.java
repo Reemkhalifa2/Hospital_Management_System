e0b0ce20d49a39d6453b180ec186abf658fbf8a3
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 12:24:06 +0400

    Add specific fields for Doctor

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -1,5 +1,13 @@
 package Entities;
 
 public class Doctor extends Person{
+    private String doctorId;
+    private String specialization;
+    private String qualification;
+    private Integer experienceYears;
+    private String departmentId;
+    private String consultationFee;
+    private List<String> availableSlots;
+    private List<String> assignedPatients;
 
 }
