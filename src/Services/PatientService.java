b17bc19ac0bf43a677534946d3da8c3f0d89e5eb
ReemALadawi <aladawi12reem6@gmail.com>
2026-05-10 15:33:53 +0400

    Add method to add a new patient with detailed input prompts

--- src/Services/PatientService.java
+++ src/Services/PatientService.java
@@ -4,13 +4,73 @@ import Entities.Patient;
 import Utilities.Constants;
 
 import java.io.Console;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 public class PatientService {
+    static Scanner scanner = new Scanner(System.in);
     private List<Patient> patients;
-    public void addPatient(Patient patient) {
+
+
+    public Patient addPatient(){
+
+        System.out.println("Enter patient id:");
+        String id = scanner.nextLine();
+
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter gender:");
+        String gender = scanner.nextLine();
+
+        System.out.println("Enter phone number:");
+        String phone = scanner.nextLine();
+
+        System.out.println("Enter date of birth (yyyy-MM-dd):");
+        String DOB = scanner.nextLine();
+        LocalDate dateOfBirth = LocalDate.parse(DOB);
+
+        System.out.println("Enter email:");
+        String email = scanner.nextLine();
+
+        System.out.println("Enter address:");
+        String address = scanner.nextLine();
+
+        System.out.println("Enter patient ID:");
+        String patientId = scanner.nextLine();
+
+        System.out.println("Enter blood group:");
+        String bloodGroup = scanner.nextLine();
+
+        System.out.println("Enter emergency contact:");
+        String emergencyContact = scanner.nextLine();
+
+        System.out.println("Enter insurance ID:");
+        String insuranceId = scanner.nextLine();
+
+        Patient patient= new Patient(
+                id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phone,
+                email,
+                address,
+                patientId,
+                bloodGroup,
+                emergencyContact,
+                LocalDate.now(),
+                insuranceId
+        );
         patients.add(patient);
         System.out.println(Constants.PATIENT_ADDED_SUCCESSFULLY);
+        return patient;
+
     }
 
     public void displayAllPatients(){
