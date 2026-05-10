95ad100756cc1e5b2bc8889c1de9b3f79372e896
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-10 11:58:47 +0400

    add new constant

--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -3,6 +3,7 @@ package Utilities;
 public class Constants {
     public static String ADD_MEDICAL_RECORD = "Add Medical Record: ";
     public static String ADD_APPOINTMENT = "Add Appointment: ";
+    public static String ENTER_INSURANCE = "Enter new Insurance: ";
 
 
 }
