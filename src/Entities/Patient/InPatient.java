844f6a09bc62aaa8cbd5074d0edb88e2c593b1e3
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 12:01:05 +0400

    Add admittingDoctorId field to InPatient class

--- src/Entities/Patient/InPatient.java
+++ src/Entities/Patient/InPatient.java
@@ -8,6 +8,7 @@ public class InPatient extends Patient{
     LocalDate dischargeDate;
     String roomNumber;
     String bedNumber;
+    String admittingDoctorId;
 
     public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
