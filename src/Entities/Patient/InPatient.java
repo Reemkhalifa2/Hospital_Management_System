ef3c148a05f78457badea5dfb9ccc99a76dc33b9
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 12:04:23 +0400

    Add fields  to InPatient constructor

--- src/Entities/Patient/InPatient.java
+++ src/Entities/Patient/InPatient.java
@@ -12,7 +12,17 @@ public class InPatient extends Patient{
     Double dailyCharges;
 
 
-    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
+    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                     String gender, String phoneNumber, String email, String address,
+                     String bloodGroup, String emergencyContact, LocalDate registrationDate,
+                     String insuranceId, List<String> allergies, LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, Double dailyCharges) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
+
+        this.admissionDate = admissionDate;
+        this.dischargeDate = dischargeDate;
+        this.roomNumber =  roomNumber;
+        this.bedNumber =  bedNumber;
+        this.admittingDoctorId  = admittingDoctorId;
+        this.dailyCharges = dailyCharges;
     }
 }
