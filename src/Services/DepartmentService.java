61eb265b1d4417557ed47720fb2cfade1e33bc67
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 10:06:01 +0400

    Add DepartmentService: manage departments

---
+++ src/Services/DepartmentService.java
@@ -0,0 +1,30 @@
+package Services;
+
+import Entities.Department;
+import Utilities.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentService {
+    List<Department> departments = new ArrayList<>();
+
+    public void addDepartment(Department department){
+        departments.add(department);
+        System.out.println(Constants.DEPARTMENT_ADDED_SUCCESSFULLY);
+    }
+
+    public void getDepartmentById(String departmentId){
+        for(Department d : departments){
+            if(d.getDepartmentId().equals(departmentId)){
+                d.displayInfo();
+                return;
+            }
+        }
+        System.out.println(Constants.DEPARTMENT_NOT_FOUND);
+    }
+
+
+
+
+}
