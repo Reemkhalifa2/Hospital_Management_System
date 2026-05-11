eb475b2a4ade38662c88e3118b67fd9e744d0910
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 12:00:26 +0400

    Add dischargeDate field to InPatient class

--- src/Entities/Patient/InPatient.java
+++ src/Entities/Patient/InPatient.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class InPatient extends Patient{
     LocalDate admissionDate;
+    LocalDate dischargeDate;
 
     public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
