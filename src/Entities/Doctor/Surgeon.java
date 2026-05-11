d73b906ad3f371fef9657540cbd15f4da23b1f35
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 10:59:03 +0400

    Add Surgeon class enhancements: include surgeriesPerformed, surgeryTypes, and operationTheatreAccess

--- src/Entities/Doctor/Surgeon.java
+++ src/Entities/Doctor/Surgeon.java
@@ -1,10 +1,24 @@
 package Entities.Doctor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Surgeon extends Doctor{
+    private Integer surgeriesPerformed;
+    private List<String> surgeryTypes;
+    private Boolean operationTheatreAccess;
 
-    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String specialization, String qualification, Integer experienceYears, String departmentId, double consultationFee) {
+
+    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender,
+                   String phoneNumber, String email, String address, String specialization, String qualification,
+                   Integer experienceYears, String departmentId, double consultationFee , Integer surgeriesPerformed ,Boolean operationTheatreAccess ,
+                   ) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, specialization, qualification, experienceYears, departmentId, consultationFee);
+            this.surgeriesPerformed = surgeriesPerformed;
+            this.operationTheatreAccess = operationTheatreAccess;
+            this.surgeryTypes = new ArrayList<>();
     }
+
+
 }
