ac4e896339f1ca4226f3b9405dfe960940dfeeb8
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 08:34:33 +0400

    Add method to add a nurse and print success message

--- src/Services/NurseService.java
+++ src/Services/NurseService.java
@@ -1,12 +1,18 @@
 package Services;
 
 import Entities.Nurse;
+import Utilities.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NurseService {
     List <Nurse> nurses = new ArrayList<>();
+
+    public void addNurse(Nurse nurse){
+        nurses.add(nurse);
+        System.out.println(Constants.NURSE_ADDED_SUCCESSFULLY);
+    }
 
 
 
