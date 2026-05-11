03f782dd658cc23f2392fca170688a58942f9ea1
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 08:31:36 +0400

    Add NurseService class to manage Nurse entities

---
+++ src/Services/NurseService.java
@@ -0,0 +1,13 @@
+package Services;
+
+import Entities.Nurse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NurseService {
+    List <Nurse> nurses = new ArrayList<>();
+
+
+
+}
