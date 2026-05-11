48b15ab71286287cdc7254aa9a6898373051962b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 11:21:17 +0400

    override displayInfo method to Consultant class

--- src/Entities/Doctor/Consultant.java
+++ src/Entities/Doctor/Consultant.java
@@ -13,4 +13,15 @@ public class Consultant extends Doctor{
     public Consultant(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String specialization, String qualification, Integer experienceYears, String departmentId, double consultationFee) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, specialization, qualification, experienceYears, departmentId, consultationFee);
     }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Online Consultation Available: " + onlineConsultationAvailable);
+        System.out.println("Consultation Duration: " + consultationDuration);
+        System.out.println("Consultation Types");
+        for(String x : consultationTypes) {
+            System.out.print(x + " , ");
+        }
+    }
 }
