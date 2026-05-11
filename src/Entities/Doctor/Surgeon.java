52849da089ec9169ca3521309c78d303ab5bfc47
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 11:04:05 +0400

    Add displayInfo method to Surgeon class

--- src/Entities/Doctor/Surgeon.java
+++ src/Entities/Doctor/Surgeon.java
@@ -12,13 +12,20 @@ public class Surgeon extends Doctor{
 
     public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender,
                    String phoneNumber, String email, String address, String specialization, String qualification,
-                   Integer experienceYears, String departmentId, double consultationFee , Integer surgeriesPerformed ,Boolean operationTheatreAccess ,
-                   ) {
+                   Integer experienceYears, String departmentId, double consultationFee , Integer surgeriesPerformed ,Boolean operationTheatreAccess) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, specialization, qualification, experienceYears, departmentId, consultationFee);
             this.surgeriesPerformed = surgeriesPerformed;
             this.operationTheatreAccess = operationTheatreAccess;
             this.surgeryTypes = new ArrayList<>();
     }
 
-
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Surgeries Performed: " + surgeriesPerformed);
+        System.out.println("Operation Theatre Access: " + operationTheatreAccess);
+        for(String x : surgeryTypes){
+            System.out.print(x + " , ");
+        }
+    }
 }
