7c697fc6070af9cfe404169f585c50add9b45439
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 12:00:53 +0400

    Add bedNumber field to InPatient class

--- src/Entities/Patient/InPatient.java
+++ src/Entities/Patient/InPatient.java
@@ -7,6 +7,7 @@ public class InPatient extends Patient{
     LocalDate admissionDate;
     LocalDate dischargeDate;
     String roomNumber;
+    String bedNumber;
 
     public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
