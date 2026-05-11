47fe7304fd60f4bab033c11e90847fa28c93a5b4
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 12:00:41 +0400

    Add roomNumber field to InPatient class

--- src/Entities/Patient/InPatient.java
+++ src/Entities/Patient/InPatient.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class InPatient extends Patient{
     LocalDate admissionDate;
     LocalDate dischargeDate;
+    String roomNumber;
 
     public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
