6b540a89a625d1832e693cf68c14ad1bd7449020
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 08:43:44 +0400

    Add method to input nurse details

--- src/Services/NurseService.java
+++ src/Services/NurseService.java
@@ -1,17 +1,78 @@
 package Services;
 
+import Entities.Doctor;
 import Entities.Nurse;
 import Utilities.Constants;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class NurseService {
+    static Scanner scanner = new Scanner(System.in);
     List <Nurse> nurses = new ArrayList<>();
 
     public void addNurse(Nurse nurse){
         nurses.add(nurse);
         System.out.println(Constants.NURSE_ADDED_SUCCESSFULLY);
+    }
+
+    public Nurse addNurses(){
+
+        System.out.println("Enter Nurse id:");
+        String nurseId = scanner.nextLine();
+
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter gender:");
+        String gender = scanner.nextLine();
+
+        System.out.println("Enter phone number:");
+        String phone = scanner.nextLine();
+
+        System.out.println("Enter date of birth (yyyy-MM-dd):");
+        String DOB = scanner.nextLine();
+        LocalDate dateOfBirth = LocalDate.parse(DOB);
+
+        System.out.println("Enter email:");
+        String email = scanner.nextLine();
+
+        System.out.println("Enter address:");
+        String address = scanner.nextLine();
+
+        System.out.println("Enter department id:");
+        String departmentId = scanner.nextLine();
+
+        System.out.println("Enter qualification:");
+        String shift = scanner.nextLine();
+
+        System.out.println("Enter shift:");
+        String qualification = scanner.nextLine();
+
+
+        Nurse nurse = new Nurse(
+                nurseId,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phone,
+                email,
+                address,
+                departmentId,
+                shift,
+                qualification
+        );
+
+        return nurse;
+
+
     }
 
 
