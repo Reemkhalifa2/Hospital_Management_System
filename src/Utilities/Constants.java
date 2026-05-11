b33b24d6bb122bc19eb15433b5c8d764bdb362c4
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 08:18:24 +0400

    add new constants

--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -12,6 +12,8 @@ public class Constants {
     public static String PATIENT_UPDATED_SUCCESSFULLY = "Patient updated successfully";
     public static String PATIENT_NOT_FOUND = "Patient not found";
     public static String DOCTOR_ADDED_SUCCESSFULLY = "Doctor added successfully";
+    public static String DOCTOR_UPDATED_SUCCESSFULLY = "Doctor updated successfully";
+    public static String DOCTOR_NOT_FOUND = "Doctor not found";
 
 
 
