eb29855b3b822743d5917c312a6ea2284eb2fee5
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 12:00:09 +0400

    Add admissionDate field to InPatient class

--- src/Entities/Patient/InPatient.java
+++ src/Entities/Patient/InPatient.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class InPatient extends Patient{
+    LocalDate admissionDate;
+
     public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
     }
