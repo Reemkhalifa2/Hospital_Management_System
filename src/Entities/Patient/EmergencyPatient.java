6ef24823480ec887b6691c3bf0505a035e17804e
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 13:48:50 +0400

    Add  attributes to EmergencyPatient class

--- src/Entities/Patient/EmergencyPatient.java
+++ src/Entities/Patient/EmergencyPatient.java
@@ -4,6 +4,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class EmergencyPatient extends Patient {
+
+    String emergencyType;
+    String arrivalMode;
+    Integer triageLevel;
+    Boolean admittedViaER;
+
     public EmergencyPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
     }
