ac4269315b0da78379e660bed9ce3aac284a3d32
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-11 11:59:49 +0400

    Add constructor to InPatient class

--- src/Entities/Patient/InPatient.java
+++ src/Entities/Patient/InPatient.java
@@ -1,4 +1,10 @@
 package Entities.Patient;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class InPatient extends Patient{
+    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String bloodGroup, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> allergies) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, bloodGroup, emergencyContact, registrationDate, insuranceId, allergies);
+    }
 }
