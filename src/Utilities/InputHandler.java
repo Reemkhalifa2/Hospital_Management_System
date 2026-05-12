c2a5c6afa030082d6f0aefc2fbbe6c392584d518
ReemALadawi <aladawi12reem6@gmail.com>
2026-05-12 22:19:54 +0400

    Add validation for  user input

--- src/Utilities/InputHandler.java
+++ src/Utilities/InputHandler.java
@@ -1,5 +1,23 @@
 package Utilities;
 
+import java.util.Scanner;
+
 public class InputHandler {
     private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getStringInput(String prompt) {
+        String input = "";
+        while (input.isEmpty()) {
+            System.out.print(prompt);
+            input = scanner.nextLine().trim();
+
+            if (input.isEmpty()) {
+                System.out.println("");
+            }
+        }
+
+        return input;
+    }
+
+
 }
