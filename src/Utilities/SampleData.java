b63cf3a3a92eaed044525c3422dac502c1dcb670
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-12 14:29:51 +0400

    Add SampleData class to load sample data for services

---
+++ src/Utilities/SampleData.java
@@ -0,0 +1,17 @@
+package Utilities;
+
+import Services.*;
+
+public class SampleData {
+    public static void loadSampleData(
+            PatientService patientService,
+            DoctorService doctorService,
+            NurseService nurseService,
+            DepartmentService departmentService,
+            AppointmentService appointmentService,
+            MedicalRecordService medicalRecordService
+
+
+
+    ) {
+}
