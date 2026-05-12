ba6d782075262374cc59e75dca4f4482c6c0b38a
ReemALadawi <aladawi12reem6@gmail.com>
2026-05-12 11:38:49 +0400

    Add utility methods for null checks in HelperUtils

---
+++ src/Utilities/HelperUtils.java
@@ -0,0 +1,12 @@
+package Utilities;
+
+public class HelperUtils {
+    public static Boolean isNull(Object obj){
+        return obj == null;
+    }
+
+    public static Boolean isNull(String str){
+        return str == null&& str.isEmpty() ;
+    }
+
+}
