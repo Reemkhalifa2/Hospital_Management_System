a540ea1997e5660ec4982edfbfe4c8478c4863c0
ReemALadawi <aladawi12reem6@gmail.com>
2026-05-13 23:15:00 +0400

    feat(nurses): display information for all nurses in getAll method

--- src/Services/NurseService.java
+++ src/Services/NurseService.java
@@ -167,6 +167,7 @@ public class NurseService implements Manageable, Searchable {
 
     @Override
     public void getAll() {
+        nurses.forEach(nurse -> nurse.displayInfo());
     }
 
     @Override
