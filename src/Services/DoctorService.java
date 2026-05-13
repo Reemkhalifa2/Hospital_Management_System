913c01cd7761a4124fdb0ae08d67853cb964631b
ReemALadawi <aladawi12reem6@gmail.com>
2026-05-13 10:46:21 +0400

    refactor add doctor

--- src/Services/DoctorService.java
+++ src/Services/DoctorService.java
@@ -25,78 +25,24 @@ public class DoctorService implements Manageable, Searchable {
         System.out.println(Constants.DOCTOR_ADDED_SUCCESSFULLY);
     }
 
-    public Doctor addDoctor() {
+    public void addDoctor() {
 
-        System.out.println("Enter first name:");
-        String firstName = scanner.nextLine();
-
-        System.out.println("Enter last name:");
-        String lastName = scanner.nextLine();
-
-        System.out.println("Enter gender:");
-        String gender = scanner.nextLine();
-
-        System.out.println("Enter phone number:");
-        String phone = scanner.nextLine();
-
-        System.out.println("Enter date of birth (yyyy-MM-dd):");
-        String DOB = scanner.nextLine();
-        LocalDate dateOfBirth = LocalDate.parse(DOB);
-
-        System.out.println("Enter email:");
-        String email = scanner.nextLine();
-
-        System.out.println("Enter address:");
-        String address = scanner.nextLine();
-
-        System.out.println("Enter specialization:");
-        String specialization = scanner.nextLine();
-
-        System.out.println("Enter qualification:");
-        String qualification = scanner.nextLine();
-
-        System.out.println("Enter years of experience:");
-        int experienceYears = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Enter department id:");
-        String departmentId = scanner.nextLine();
-
-        System.out.println("Enter consultation fee:");
-        double consultationFee = Double.parseDouble(scanner.nextLine());
-
-        List<String> availableSlots = new ArrayList<>();
-
-        System.out.println("Do you want to add available slots? (yes/no)");
-        String addSlots = scanner.nextLine();
-
-        if (addSlots.equalsIgnoreCase("yes")) {
-
-            System.out.println("Enter slots separated by commas:");
-            String slotsInput = scanner.nextLine();
-
-            availableSlots = Arrays.asList(slotsInput.split(","));
-        }
-
-        List<String> assignedPatients = new ArrayList<>();
-
-        Doctor doctor = new Doctor(
-                HelperUtils.generateId("Dc."),
-                firstName,
-                lastName,
-                dateOfBirth,
-                gender,
-                phone,
-                email,
-                address,
-                specialization,
-                qualification,
-                experienceYears,
-                departmentId,
-                consultationFee
-
-        );
-
-        return doctor;
+        String id    = HelperUtils.generateId("DOC-",4);
+        String firstName    = InputHandler.getStringInput("First Name: ");
+        String lastName    = InputHandler.getStringInput("Last Name: ");
+        LocalDate dob = InputHandler.getDateInput("Date of Birth");
+        String gender = InputHandler.getStringInput("Gender: ");
+        String phone  = InputHandler.getStringInput("Phone: ");
+        String email  = InputHandler.getStringInput("Email: ");
+        String address  = InputHandler.getStringInput("Address: ");
+        String specialization   = InputHandler.getStringInput("Specialization: ");
+        String qualification   = InputHandler.getStringInput("Qualification: ");
+        int    experience    = InputHandler.getIntInput("Experience (years): ", 0, 60);
+        String deptId   = InputHandler.getStringInput("Department ID: ");
+        double fee    = InputHandler.getDoubleInput("Consultation Fee: ");
+        Doctor d = new Doctor(id, firstName, lastName, dob, gender, phone, email, address,
+                specialization, qualification, experience, deptId, fee);
+        add(d);
     }
 
     public void addDoctor(String name, String specialization, String phone){
